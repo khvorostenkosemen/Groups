4120ac3ffdf478c4a6e803647b9e83413285b256
AkulasIKEA <semenhvorostenko@gmail.com>
2024-03-27 23:18:23 +0200

    Додав 2 тестових класа до 2 гілки

---
+++ Tests/TeacherEntityTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Teachers.Tests
+{
+    [TestClass]
+    public class TeacherEntityTests
+    {
+        [TestMethod]
+        public void TeacherEntity_Initialization_ShouldSucceed()
+        {
+            // Arrange
+            var teacher = new Teachers.Entities.TeacherEntity
+            {
+                Name = "Василь Миколайович",
+                Age = 55,
+                Department = "Фізика"
+            };
+
+            // Act
+            string expectedName = "Василь Миколайович";
+            int expectedAge = 55;
+            string expectedDepartment = "Фізика";
+
+            // Assert
+            Assert.AreEqual(expectedName, teacher.Name);
+            Assert.AreEqual(expectedAge, teacher.Age);
+            Assert.AreEqual(expectedDepartment, teacher.Department);
+        }
+    }
+}
