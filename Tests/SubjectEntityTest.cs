f0b2788c38c7779a42efbaa0219470363ba6545d
AkulasIKEA <semenhvorostenko@gmail.com>
2024-03-27 23:15:27 +0200

    Додав тестові класи в1 гілку

---
+++ Tests/SubjectEntityTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Subjects.Tests
+{
+    [TestClass]
+    public class SubjectEntityTests
+    {
+        [TestMethod]
+        public void SubjectEntity_Initialization_ShouldSucceed()
+        {
+            // Arrange
+            var subject = new Subjects.Entities.SubjectEntity
+            {
+                SubjectName = "Математика",
+                TeacherName = "Валерія Манік"
+            };
+
+            // Act
+            string expectedSubjectName = "Математика";
+            string expectedTeacherName = "Валерія Манік";
+
+            // Assert
+            Assert.AreEqual(expectedSubjectName, subject.SubjectName);
+            Assert.AreEqual(expectedTeacherName, subject.TeacherName);
+        }
+    }
+}
